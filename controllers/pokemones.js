0a05b327b5f7cbfb3807568a84455f82e9bb227f
Javier Fernández <javierffischer@gmail.com>
2022-10-05 14:16:58 -0300

    Se agregan controles con libreria yup a pokemones.js

--- controllers/pokemones.js
+++ controllers/pokemones.js
@@ -1,4 +1,5 @@
 const db = require("../db/index");
+const yup = require("yup");
 
 const obtener = async (req, res) => {
   try {
@@ -34,33 +35,77 @@ const agregar = async (req, res) => {
       move1,
       move2,
     } = req.body;
-    
-    console.log(req.body)
 
     let newPokemon = {
-        idpokemon,
-        name,
-        img,
-        type,
-        type2,
-        weight,
-        height,
-        description,
-        hp,
-        atk,
-        def,
-        satk,
-        sdef,
-        spd,
-        bckcolor,
-        bckcolor2,
+      idpokemon,
+      name,
+      img,
+      type,
+      type2,
+      weight,
+      height,
+      description,
+      hp,
+      atk,
+      def,
+      satk,
+      sdef,
+      spd,
+      bckcolor,
+      bckcolor2,
     };
-    
+
     let newMoves = {
       idpokemon,
       move1,
       move2,
     };
+
+    let schema = yup.object().shape({
+      idpokemon: yup.number().integer().required().positive(),
+      name: yup.string().required(),
+      img: yup.string().required(),
+      type: yup.string().required(),
+      type2: yup.string(),
+      weight: yup.number().required().positive(),
+      height: yup.number().required().positive(),
+      description: yup.string().required(),
+      hp: yup.number().integer().required().positive().max(200),
+      atk: yup.number().integer().required().positive().max(200),
+      def: yup.number().integer().required().positive().max(200),
+      satk: yup.number().integer().required().positive().max(200),
+      sdef: yup.number().integer().required().positive().max(200),
+      spd: yup.number().integer().required().positive().max(200),
+      bckcolor: yup.string().required(),
+      bckcolor: yup.string(),
+    });
+
+    schema.validate(newPokemon).catch(function (err) {
+      err.name;
+      err.errors;
+      if (err.errors) {
+        return res.status(400).json({
+          data: err.errors,
+          message: "Dates are invalid",
+          success: false,
+        });
+      }
+    });
+
+    //Este control se deja porque revisa en la base de datos que no exista el id de pokemon.
+
+    const verifyIdPokemon = await db.query(
+      "select * from poketabla where idpokemon = $1",
+      [idpokemon]
+    );
+
+    if (verifyIdPokemon.rowCount > 0) {
+      return res.status(400).json({
+        data: [],
+        message: "Id number is already used.",
+        success: false,
+      });
+    }
 
     await db.query(
       "insert into poketabla (idpokemon, name, img, type, type2, weight, height, description, hp, atk, def, satk, sdef, spd, bckcolor, bckcolor2) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16)",
@@ -84,15 +129,14 @@ const agregar = async (req, res) => {
       ]
     );
 
-
     await db.query(
       "insert into datamoves(idpokemondatamoves, move1, move2) values($1,$2,$3)",
       [idpokemon, move1, move2]
     );
 
     return res
-    .status(200)
-    .json({data: [newPokemon, newMoves], message: "Success"})
+      .status(200)
+      .json({ data: [newPokemon, newMoves], message: "Success" });
   } catch (error) {
     console.error(error);
   }
